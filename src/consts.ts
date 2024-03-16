613fe66dcfaef186af4ea1bf11333fbeed065135
yutakobaayshidev <hi@yutakobayashi.dev>
2024-03-17 00:04:39 +0900

    init

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "YUTA STUDIO";
+export const SITE_DESCRIPTION =
+	"プログラミング、ガジェット、AI、音楽などなどなんでも書くテックブログ";
