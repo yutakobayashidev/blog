f8407344773b349ea5e416ed8240c72a698312a6
yutakobaayshidev <hi@yutakobayashi.dev>
2024-03-18 23:00:40 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: "https://studio.yutakobayashi.dev",
+	integrations: [mdx(), sitemap(), tailwind()],
 });
